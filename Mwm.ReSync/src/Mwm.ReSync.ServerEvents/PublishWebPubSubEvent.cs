2fbb71dfa3711ced7f91806532aacc653f995eac
Mark Michalek <mark.michalek@blackstone.com>
2024-11-21 11:51:18 -0500

    Added AF

--- Mwm.ReSync/src/Mwm.ReSync.ServerEvents/PublishWebPubSubEvent.cs
+++ Mwm.ReSync/src/Mwm.ReSync.ServerEvents/PublishWebPubSubEvent.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
 
 namespace Mwm.ReSync.ServerEvents;
 
@@ -12,6 +13,15 @@ public class PublishWebPubSubEvent
     public PublishWebPubSubEvent(ILogger<PublishWebPubSubEvent> logger)
     {
         _logger = logger;
+    }
+    
+    [FunctionName("WebPubSubTrigger")]
+    public static void Run(
+        [WebPubSubTrigger("messages", WebPubSubEventType.User, "message")] UserEventRequest request, ILogger log)
+    {
+        log.LogInformation($"Request from: {request.ConnectionContext.UserId}");
+        log.LogInformation($"Request message data: {request.Data}");
+        log.LogInformation($"Request message dataType: {request.DataType}");
     }
 
     // [FunctionName("WebPubSubFunction")] 
